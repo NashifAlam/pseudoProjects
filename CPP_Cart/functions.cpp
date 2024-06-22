ce8077e9582829774fa472bf3e1eb252960237d3
Nashif Alam <work.nashifalam@gmail.com>
2024-06-22 19:06:39 +0530

    Checkout Cart creation of File modules

---
+++ CPP_Cart/functions.cpp
@@ -0,0 +1,54 @@
+#include "header.h"
+#include "cart.h"
+#include "bank.h"
+
+void stall()
+{
+	__fpurge(stdin);
+	std::cout << "\n\nPress enter to continue\n";
+	getchar();
+	return;
+}
+
+void menu()
+{
+	system("clear");
+	std::cout<< "Please select your preferences " 
+		<< "\nA: Add item to cart"
+		<<"\nR: Remove item from cart"
+		<<"\nB: View the total Bill"
+		<<"\nC: Checkout\nE: Exit\n"
+		<< "Enter your selection : "; 
+}
+
+bool checkout(Cart &cart)
+{
+	system("clear");
+	Bank bank;
+	system("clear");
+	std::cout << "This is checkout section\n"
+		<<"------------------------\n\n";	
+	
+	if(cart.item.empty()){
+		std::cout << "Cart is empty\nPlease use Add to Cart for adding items";
+		stall();
+		return false;}
+
+	if(cart.totalCharges < 1){
+		std::cout << "Total bill is zero\nCannot proceed to checkout";
+		stall();
+		return false;}
+
+	if(bank.vendorTransaction(cart.totalCharges))
+	{
+		stall();
+		system("clear");
+		std::cout<<"Redirecting to Merchant Site.";	
+		sleep(1);	
+		system("clear");
+		return true;
+	}
+	return false;
+}
+
+
